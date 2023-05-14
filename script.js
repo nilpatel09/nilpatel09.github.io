196556ed55c2372d3dff7c4b8960d747aadd52c1
nilpatel09 <124824455+nilpatel09@users.noreply.github.com>
2023-05-14 09:29:48 -0500

    new files

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get the contact button element
+const contactButton = document.getElementById('contact');
+
+// Add a click event listener to the contact button
+contactButton.addEventListener('click', () => {
+	// Replace with your own contact page URL
+	window.location.href = 'https://example.com/contact';
+});
+
+// Get the LinkedIn button element
+const linkedinButton = document.getElementById('linkedin');
+
+// Add a click event listener to the LinkedIn button
+linkedinButton.addEventListener('click', () => {
+	// Replace with your own LinkedIn profile URL
+	window.location.href = 'https://linkedin.com/in/your-profile';
+});
