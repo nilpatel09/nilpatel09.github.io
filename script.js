94e19ee5e6d949e0ad503c9798d0f5a74a3ffa82
nilpatel09 <124824455+nilpatel09@users.noreply.github.com>
2023-05-14 09:51:52 -0500

    updated

--- script.js
+++ script.js
@@ -1,17 +1,83 @@
-// Get the contact button element
-const contactButton = document.getElementById('contact');
+const chatbot = document.querySelector("#chatbot");
+const chatbotHeader = document.querySelector("#chatbot-header");
+const chatbotClose = document.querySelector("#chatbot-close");
+const chatbotBody = document.querySelector("#chatbot-body");
+const chatbotInput = document.querySelector("#chatbot-input");
 
-// Add a click event listener to the contact button
-contactButton.addEventListener('click', () => {
-	// Replace with your own contact page URL
-	window.location.href = 'https://example.com/contact';
-});
+// Function to create a new message element
+function createMessageElement(message, sender) {
+  const messageWrapper = document.createElement("div");
+  messageWrapper.classList.add("chatbot-message");
 
-// Get the LinkedIn button element
-const linkedinButton = document.getElementById('linkedin');
+  const avatar = document.createElement("img");
+  avatar.src = sender === "user" ? "user-avatar.png" : "chatbot-avatar.png";
+  avatar.alt = sender;
 
-// Add a click event listener to the LinkedIn button
-linkedinButton.addEventListener('click', () => {
-	// Replace with your own LinkedIn profile URL
-	window.location.href = 'https://linkedin.com/in/your-profile';
-});
+  const messageText = document.createElement("div");
+  messageText.classList.add("message-text");
+  messageText.textContent = message;
+
+  messageWrapper.appendChild(avatar);
+  messageWrapper.appendChild(messageText);
+
+  chatbotBody.appendChild(messageWrapper);
+}
+
+// Function to handle user input
+function handleUserInput(e) {
+  if (e.keyCode === 13) {
+    const message = e.target.value;
+    createMessageElement(message, "user");
+    e.target.value = "";
+    // Add logic here to send message to a backend server and get a response
+    const response = "This is a sample response from the chatbot!";
+    createMessageElement(response, "chatbot");
+  }
+}
+
+// Function to toggle the chatbot
+function toggleChatbot() {
+  chatbot.classList.toggle("open");
+  chatbotInput.focus();
+}
+
+// Add event listeners
+chatbotHeader.addEventListener("click", toggleChatbot);
+chatbotClose.addEventListener("click", toggleChatbot);
+chatbotInput.addEventListener("keydown", handleUserInput);
+
+
+
+
+
+
+
+
+
+
+
+// Function to toggle the navigation menu
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("open");
+  }
+  
+  // Sliding images in background
+  const images = [
+    "background-image-1.jpg",
+    "background-image-2.jpg",
+    "background-image-3.jpg",
+    "background-image-4.jpg",
+    "background-image-5.jpg"
+  ];
+  let currentIndex = 0;
+  setInterval(() => {
+    const background = document.querySelector(".background");
+    currentIndex = (currentIndex + 1) % images.length;
+    background.style.backgroundImage = `url(${images[currentIndex]})`;
+  }, 5000);
+  
+  // Add event listeners
+  const menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", toggleMenu);
+  
